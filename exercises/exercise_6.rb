06de75d3442591a21113f3acb1f2b5d8abb65540
jjyh <janejyh@gmail.com>
2023-12-21 21:20:46 -0500

    ex 6,7 - create and restrict records, link between one to many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -10,7 +10,12 @@ puts "----------"
 
 # Your code goes here ...
 # Add the following code directly inside the Store model (class): has_many :employees
-
 # Add the following code directly inside the Employee model (class): belongs_to :store
 # Add some data into employees. Here's an example of one (note how it differs from how you create stores): @store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
 # Go ahead and create some more employees using the create method. You can do this by making multiple calls to create (like you have before.) No need to assign the employees to variables though. Create them through the @store# instance variables that you defined in previous exercises. Create a bunch under @store1 (Burnaby) and @store2 (Richmond). Eg: @store1.employees.create(...).
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: 'Luke', last_name: 'Skywalker', hourly_rate: 50)
+@store1.employees.create(first_name: 'Obiwan', last_name: 'Kenobi', hourly_rate: 45)
+@store2.employees.create(first_name: 'Darth', last_name: 'Vadar', hourly_rate: 15)
+@store2.employees.create(first_name: 'Darth', last_name: 'Sidious', hourly_rate: 35)
--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,9 @@
 class Employee < ActiveRecord::Base
+  belongs_to :store
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: true, inclusion: 40..200 #{ greater_than: 40, less_than: 3000 }
+  validates :store_id,    presence: true
 
 end
